5511072e7bd7195b5ae7b78bfffe300a831b535b
usuario <usuario@DebianServer10.ejemplo.dominio>
2021-05-08 17:45:40 -0500

    mejoras

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,12 +14,25 @@ class ArticlesController < ApplicationController
 	
 #POST /articles
 	def create
+	#@article.valid
+	#@article.invalid
 		@article = Article.new(title: params[:article][:title], body:params[:article][:body], visits_count: 0)
-		@article.save
-		redirect_to @article	
+		if @article.save
+			redirect_to @article	
+		else
+			render :new
+		end
 	end
+	
+#DELETE /articles/:id
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to articles_path
+	end	
+	
 #PUT /articles/:id
 	def update
-
+		#@article.update_attributes({title:"Nuevo titulo", body:"", visits_count: 0})
 	end
 end
